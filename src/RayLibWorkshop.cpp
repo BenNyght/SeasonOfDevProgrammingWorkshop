29525aae7ea532fd58d58b0a389ebb8967345faa
Nyght <mail.benjaminfinlay@gmail.com>
2025-03-19 21:03:03 +1000

    OBJECTIVE 2 - Setting up RayLib

--- src/RayLibWorkshop.cpp
+++ src/RayLibWorkshop.cpp
@@ -1,8 +1,22 @@
 ﻿
-#include <iostream>
+#include "raylib.h"
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    InitWindow(800, 450, "Programming Workshop!");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+
+        Rectangle rec { 30, 30, 30, 30 };
+        Color color { 35, 121, 174, 255 };
+        DrawRectangleRec(rec, color);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
