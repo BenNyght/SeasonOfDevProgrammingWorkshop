f300ad9ffc25ab079260ad8f25aa622919556304
Nyght <mail.benjaminfinlay@gmail.com>
2025-03-19 21:03:34 +1000

    OBJECTIVE 3 - Moving the Player

--- src/RayLibWorkshop.cpp
+++ src/RayLibWorkshop.cpp
@@ -6,12 +6,33 @@ int main()
     InitWindow(800, 450, "Programming Workshop!");
     SetTargetFPS(60);
 
+    Rectangle rec { 30, 30, 30, 30 };
+    Color color { 35, 121, 174, 255 };
+
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
 
-        Rectangle rec { 30, 30, 30, 30 };
-        Color color { 35, 121, 174, 255 };
+        ClearBackground({30, 30, 30, 255});
+
+        float movementSpeed = 2.5f;
+        if (IsKeyDown(KEY_D)) 
+        {
+            rec.x += movementSpeed;
+        }
+        if (IsKeyDown(KEY_A)) 
+        {
+            rec.x -= movementSpeed;
+        }
+        if (IsKeyDown(KEY_W)) 
+        {
+            rec.y -= movementSpeed;
+        }
+        if (IsKeyDown(KEY_S)) 
+        {
+            rec.y += movementSpeed;
+        }
+
         DrawRectangleRec(rec, color);
 
         EndDrawing();
